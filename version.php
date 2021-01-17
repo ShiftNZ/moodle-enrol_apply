dc8162f1d4ad26ace18b26295a40066e1dfcf783
Chris <chris.gr.clark@gmail.com>
2021-01-18 08:59:10 +1300

    renamed and bumped the version

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019120400; // TODO: reversion to 2021
+$plugin->version = 2021011800;
 $plugin->requires = 2011080100;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'Enrolment upon approval plugin Version 3.7-a'; // TODO: rename
+$plugin->release = 'Enrolment upon approval plugin (Customized for Hospice Waikato)';
 $plugin->component = 'enrol_apply'; // TODO: refactor
 // TODO: Add event hook to add newly confirmed users to the waitlist
